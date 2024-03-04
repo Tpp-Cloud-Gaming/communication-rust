f693668a88f2119414e2d5afe049a203fb307a45
Franco <fgazzola@fi.uba.ar>
2024-03-04 16:45:30 -0300

    fix: Change keyboard input to winapi lib. Co-authored-by: juanchycc <jcaserio@fi.uba.ar>

--- src/output/button_controller.rs
+++ src/output/button_controller.rs
@@ -1,8 +1,11 @@
 use super::output_const::*;
+use std::mem;
 use std::sync::Arc;
 use webrtc::data_channel::data_channel_message::DataChannelMessage;
 use webrtc::data_channel::RTCDataChannel;
-use winput::{Button, Vk};
+use winapi::um::winuser::*;
+use winput::Button;
+
 
 pub struct ButtonController {}
 
@@ -20,10 +23,10 @@ impl ButtonController {
                 //let (action, key) = get_action_and_key(&msg.data);
                 match action {
                     PRESS_KEYBOARD_ACTION => {
-                        winput::press(unsafe { Vk::from_u8(key) });
+                        send_input_key(key as i32, false);
                     }
                     RELEASE_KEYBOARD_ACTION => {
-                        winput::release(unsafe { Vk::from_u8(key) });
+                        send_input_key(key as i32, true);
                     }
                     PRESS_MOUSE_ACTION => {
                         let button = get_mouse_button(key);
@@ -48,5 +51,25 @@ fn get_mouse_button(key: u8) -> Button {
         3 => Button::X1,
         4 => Button::X2,
         _ => Button::Left, //TODO: fix this
+    }
+}
+
+
+
+pub fn send_input_key(virtual_key: i32, up: bool) {
+    unsafe {
+        let mut input = INPUT {
+            type_: INPUT_KEYBOARD,
+            u: std::mem::zeroed(),
+        };
+        *input.u.ki_mut() = KEYBDINPUT {
+            wVk: virtual_key as u16,
+            dwFlags: if up { KEYEVENTF_KEYUP } else { 0 },
+            dwExtraInfo: 1,
+            wScan: KEYEVENTF_EXTENDEDKEY as u16,
+            time: 0,
+        };
+
+        SendInput(1, &mut input, mem::size_of::<INPUT>() as i32);
     }
 }
--- src/video/video_capture.rs
+++ src/video/video_capture.rs
@@ -35,37 +35,37 @@ unsafe extern "system" fn enumerate_callback(hwnd: HWND, lparam: LPARAM) -> BOOL
 pub fn run(tx_video: Sender<Vec<u8>>) {
     // Initialize GStreamer
     gstreamer::init().unwrap();
-    sleep(Duration::from_secs(30));
-    let mut hwnds: Vec<(HWND, String, String)> = Vec::new();
-    unsafe { EnumWindows(Some(enumerate_callback), &mut hwnds as *mut _ as LPARAM)};
+   // sleep(Duration::from_secs(30));
+    // let mut hwnds: Vec<(HWND, String, String)> = Vec::new();
+    // unsafe { EnumWindows(Some(enumerate_callback), &mut hwnds as *mut _ as LPARAM)};
 
-    for (count, element) in hwnds.iter().enumerate() {        
-        println!("[{}] HWND: {:?}, Class Name:  {}, Window Text: {}", count, element.0, element.1, element.2);
-    }
+    // for (count, element) in hwnds.iter().enumerate() {        
+    //     println!("[{}] HWND: {:?}, Class Name:  {}, Window Text: {}", count, element.0, element.1, element.2);
+    // }
     
 
-    println!("Please enter a number:");
+    //println!("Please enter a number:");
 
-    let mut input = String::new();
+   // let mut input = String::new();
 
-    //Read input from the user
-    io::stdin().read_line(&mut input)
-        .expect("Failed to read line");
+    // //Read input from the user
+    // io::stdin().read_line(&mut input)
+    //     .expect("Failed to read line");
 
-    // Parse the input string into an integer
-    let number: usize = match input.trim().parse() {
-        Ok(num) => num,
-        Err(_) => {
-            println!("Invalid input, please enter a valid number.");
-            return; // Exit the program or handle the error as appropriate
-        }
-    };
+    // // Parse the input string into an integer
+    // let number: usize = match input.trim().parse() {
+    //     Ok(num) => num,
+    //     Err(_) => {
+    //         println!("Invalid input, please enter a valid number.");
+    //         return; // Exit the program or handle the error as appropriate
+    //     }
+    // };
 
-   let selected = hwnds.get(number).unwrap();
-   let window_handle = selected.0 as u64;
-   println!("You selected: {}", selected.2);
+   //let selected = hwnds.get(number).unwrap();
+   let window_handle = 0 as u64;
+   //println!("You selected: {}", selected.2);
     
-   let new_framerate= gstreamer::Fraction::new(200, 1);
+   let new_framerate= gstreamer::Fraction::new(60, 1);
    let caps = gstreamer::Caps::builder("video/x-raw")
        .field("framerate", new_framerate)
        .build();

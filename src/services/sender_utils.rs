864732a4a34c68ec392ab430d09d9fc4aef91cf2
juanchycc <jcaserio@fi.uba.ar>
2024-05-25 14:25:48 -0300

    feat: adding shortcut game support

--- src/services/sender_utils.rs
+++ src/services/sender_utils.rs
@@ -19,9 +19,24 @@ use winapi::{
 
 pub fn initialize_game(game_path: &str) -> Result<u32, Error> {
     // TODO: Check tokio option. Handle the error non generically
-    match Command::new(game_path).spawn() {
-        Ok(child) => Ok(child.id()),
-        Err(_) => Err(Error::new(ErrorKind::Other, "Error initializing game")),
+    if (game_path.ends_with(".exe")) {
+        match Command::new(game_path).spawn() {
+            Ok(child) => Ok(child.id()),
+            Err(_) => Err(Error::new(ErrorKind::Other, "Error initializing game")),
+        }
+    } else if game_path.ends_with(".lnk") || game_path.ends_with(".url") {
+        match Command::new("cmd")
+            .args(&["/c", "START", "", game_path])
+            .spawn()
+        {
+            Ok(child) => Ok(child.id()),
+            Err(_) => Err(Error::new(ErrorKind::Other, "Error initializing game")),
+        }
+    } else {
+        Err(Error::new(
+            ErrorKind::Other,
+            "Error type of file not supported",
+        ))
     }
 }
 
@@ -59,7 +74,7 @@ unsafe extern "system" fn enumerate_callback(hwnd: HWND, lparam: LPARAM) -> BOOL
             class_name_as_str.to_string(),
             window_text_as_str.to_string(),
             process_id,
-            path_name
+            path_name,
         ));
     }
 
@@ -74,9 +89,8 @@ pub fn select_game_window(game_path: &str) -> u64 {
         let mut hwnds: Vec<(HWND, String, String, DWORD, String)> = Vec::new();
         unsafe { EnumWindows(Some(enumerate_callback), &mut hwnds as *mut _ as LPARAM) };
 
-        
         // Inside a while to wait for the window of the game to start
-    
+
         for (count, element) in hwnds.iter().enumerate() {
             // println!(
             //     "[{}] PID: {:?}, Class Name:  {}, Window Text: {}, Game Path [{}] [{}]",
@@ -98,40 +112,56 @@ pub fn select_game_window(game_path: &str) -> u64 {
     window_handle
 }
 
-pub fn path_name( pid:  DWORD) -> String
-{
-
+pub fn path_name(pid: DWORD) -> String {
     let mut path_name = String::new();
     let mut cb_needed: DWORD = 0;
-    let mut h_mods: [HMODULE; 1024] =  [0u8 as HMODULE; 1024];
+    let mut h_mods: [HMODULE; 1024] = [0u8 as HMODULE; 1024];
 
     // Get a handle to the process.
-    let h_process = unsafe { OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ,winapi::shared::minwindef::FALSE, pid) };
+    let h_process = unsafe {
+        OpenProcess(
+            PROCESS_QUERY_INFORMATION | PROCESS_VM_READ,
+            winapi::shared::minwindef::FALSE,
+            pid,
+        )
+    };
 
     if h_process == winapi::shared::ntdef::NULL {
         println!("ERROR");
     }
 
-
-   // Get a list of all the modules in this process.
-    if unsafe { EnumProcessModulesEx(h_process, h_mods.as_mut_ptr(),  mem::size_of_val(&h_mods) as DWORD, &mut cb_needed, winapi::um::psapi::LIST_MODULES_DEFAULT) == TRUE }
-    {
-        
+    // Get a list of all the modules in this process.
+    if unsafe {
+        EnumProcessModulesEx(
+            h_process,
+            h_mods.as_mut_ptr(),
+            mem::size_of_val(&h_mods) as DWORD,
+            &mut cb_needed,
+            winapi::um::psapi::LIST_MODULES_DEFAULT,
+        ) == TRUE
+    } {
         let module_count = (cb_needed / mem::size_of::<HMODULE>() as DWORD) as usize;
-            for i in 0..module_count {
-                let mut sz_mod_name = [0u16; winapi::shared::minwindef::MAX_PATH];
+        for i in 0..module_count {
+            let mut sz_mod_name = [0u16; winapi::shared::minwindef::MAX_PATH];
 
-                // Get the full path to the module's file.
-                if unsafe { GetModuleFileNameExW(h_process, h_mods[i], sz_mod_name.as_mut_ptr(), sz_mod_name.len() as DWORD) } != 0 {
-                    // Print the module name and handle value.
-                    let module_name = String::from_utf16(&sz_mod_name).unwrap();
-                    let trimmed_name = module_name.trim_matches(char::from(0));
-                    if trimmed_name.ends_with(".exe") || trimmed_name.ends_with(".EXE")  {
-                        path_name = trimmed_name.to_owned();
-                    }
-                    
+            // Get the full path to the module's file.
+            if unsafe {
+                GetModuleFileNameExW(
+                    h_process,
+                    h_mods[i],
+                    sz_mod_name.as_mut_ptr(),
+                    sz_mod_name.len() as DWORD,
+                )
+            } != 0
+            {
+                // Print the module name and handle value.
+                let module_name = String::from_utf16(&sz_mod_name).unwrap();
+                let trimmed_name = module_name.trim_matches(char::from(0));
+                if trimmed_name.ends_with(".exe") || trimmed_name.ends_with(".EXE") {
+                    path_name = trimmed_name.to_owned();
                 }
             }
+        }
     }
 
     return path_name;

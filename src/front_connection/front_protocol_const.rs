5cc678a811e538a47ffe8f825f04b9936246eb58
juanchycc <jcaserio@fi.uba.ar>
2024-06-29 12:39:02 -0300

    qf

--- src/front_connection/front_protocol_const.rs
+++ src/front_connection/front_protocol_const.rs
@@ -1,4 +1,4 @@
-pub const FRONT_IP: &str = "127.0.0.1";
+pub const FRONT_IP: &str = "127.0.0.1:";
 pub const FRONT_PORT: &str = "2930";
 pub const START_OFFERING_MSG: &str = "startOffering";
 pub const START_GAME_MSG: &str = "startGameWithUser";
